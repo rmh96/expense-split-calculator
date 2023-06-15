71ecc38c2cb496836a41d29b392dd123005ef25a
rmh96 <100729581+rmh96@users.noreply.github.com>
2023-06-15 11:19:36 +0530

    Added meta & development progress status

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,12 +4,10 @@ import { useExpenseSplitContext } from "../../context/Store";
 const Header = () => {
   const { resetApp } = useExpenseSplitContext();
   return (
-    <h1
-      className="cursor-pointer py-5 w-full flex justify-center text-2xl border border-blue-500"
-      onClick={resetApp}
-    >
-      Expense Split Calculator
-    </h1>
+    <div className="flex flex-col items-center cursor-pointer py-5 w-full flex justify-center text-2xl border border-blue-500">
+      <h1 onClick={resetApp}>Expense Split Calculator</h1>
+      <span style={{ fontSize: "13px" }}>@Still in development...</span>
+    </div>
   );
 };
 
